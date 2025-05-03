2da7595d6d1ca713c2499c09c71df4ed2652fe25
Shivedra123 <Shivendra@gmail.com>
2025-05-03 21:28:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//var ko pado
+console.log('shivendra ki bike pulser')
+
+var age = 16;
+console.log(age)
+age = "shivendra"
+console.log(age)
+
+//let ko pado
+let mob_name = "oppo"
+console.log(mob_name)
+
+//const
+const umar = 18;
+5
+console.log(umar)
+
+
+
+//numbers
+let num = 2009;
+console.log(num + 1 )
+
+//aur baaki ka kal karte hai light nhi hai
